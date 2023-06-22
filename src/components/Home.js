2cdd18930ee29db8da0b962a4c4145d386f33a95
ErKunalKumar <122048688+ErKunalKumar@users.noreply.github.com>
2023-06-22 22:33:03 +0530

    3rd day

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,20 @@ import react from "react";
 function Home() {
   return (
     <div>
+      <div className="add-to-cart">
+        <img src="../images/ecart.jpg" alt="ecart" />
+      </div>
       <h1>Home Component</h1>
       <div className="cart-wrapper">
         <div className="img-wrapper item">
-          <img src="" />
+          <img src="../images/background.jpg" alt="images1" />
+        </div>
+        <div className="txt-wrapper item">
+          <span>I-Phone</span>
+          <span>Price:$1000.00</span>
+        </div>
+        <div className="btn-wrapper item">
+          <button>Add To Cart</button>
         </div>
       </div>
     </div>
